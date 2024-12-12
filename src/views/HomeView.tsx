e8255d1afa7d35973360536802114b35486e84b3
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2024-12-12 22:21:35 +0200

    Added base

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -4,11 +4,14 @@ export const HomeView = () => {
   return (
     <div className="bg-background h-screen grid grid-cols-2">
       <div></div>
-      <div className="flex flex-col justify-center items-center">
-        <div className="flex">
-          <Button />
-          <Button />
+      <div className="flex flex-col justify-center items-center gap-20 bg-[#CEE0F3]">
+        <div className="flex w-full justify-evenly">
+          <Button size={"lg"}>Sign in</Button>
+          <Button size={"lg"}>Sign up</Button>
         </div>
+        <Button variant={"link"} size={"lg"}>
+          Log in as Admin
+        </Button>
       </div>
     </div>
   );
