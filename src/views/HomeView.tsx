d7285dc4fe247630e5270f8cede9f987aa207d06
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2024-12-12 22:27:15 +0200

    Finished Home Page

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,13 +1,22 @@
 import { Button } from "../components/ui/button";
-
+import LOGO from "../assets/logo.png";
+import { useNavigate } from "react-router-dom";
 export const HomeView = () => {
+  const navigate = useNavigate();
   return (
     <div className="bg-background h-screen grid grid-cols-2">
-      <div></div>
-      <div className="flex flex-col justify-center items-center gap-20 bg-[#CEE0F3]">
+      <div className="flex flex-col justify-center items-center gap-5">
+        <img src={LOGO} className="w-[500px]" />
+        <p className="text-primary text-3xl">Project Submission Portal</p>
+      </div>
+      <div className="flex flex-col justify-center items-center gap-10 bg-[#CEE0F3]">
         <div className="flex w-full justify-evenly">
-          <Button size={"lg"}>Sign in</Button>
-          <Button size={"lg"}>Sign up</Button>
+          <Button size={"lg"} onClick={() => navigate("signin")}>
+            Sign in
+          </Button>
+          <Button size={"lg"} onClick={() => navigate("signup")}>
+            Sign up
+          </Button>
         </div>
         <Button variant={"link"} size={"lg"}>
           Log in as Admin
