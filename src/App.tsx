6da2e7687a1d2ba01cdc24e848c1ac5bc3161a03
maryam eldaly <maryam.eldaly212@gmail.com>
2024-12-13 18:59:16 +0200

    Add/Update Theme(admin)

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { Routes, Route } from "react-router-dom";
 import UserMainView from "./views/UserMainView";
 import {Login} from "./views/Login"
 import {Signup} from "./views/Signup"
+import { HomeView } from "./views/HomeView";
+import AddThemePage from "./views/AddTheme_admin";
+import UpdateTheme from "./views/UpdateTheme_admin";
 
 
 const App = () => {
@@ -13,8 +16,9 @@ const App = () => {
       <Route path="/" element={<UserMainView />} />
       <Route path="/signin" element={<Login/>} />
       <Route path="/signup" element={<Signup/>} />
-
-
+      <Route path="/HomeView" element={<HomeView/>} />
+      <Route path="/AddTheme" element={<AddThemePage/>} />
+      <Route path="/UpdateTheme" element={<UpdateTheme/>} />
       {/* Redirect unmatched routes to Home */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
