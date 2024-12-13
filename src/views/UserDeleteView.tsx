b42d6d0bdc28e472a1b11774e63934d4b7ffedc0
Youssef ElAnsary <youssefmohamednbis@gmail.com>
2024-12-13 21:24:09 +0200

    3adelt el delete type shi

--- src/views/UserDeleteView.tsx
+++ src/views/UserDeleteView.tsx
@@ -7,36 +7,38 @@ import { faTrash } from "@fortawesome/free-solid-svg-icons";
 const columns = ["ID", "Name", "Budget", "Status", "Delete"];
 
 export const UserDeleteView: React.FC = () => {
-  const [data, setData] = useState([
-    { id: 1, name: "Reusable", budget: "10000.0", status: "pending" },
-    { id: 4, name: "Reusable", budget: "10000.0", status: "pending" },
-    { id: 5, name: "Reusable", budget: "10000.0", status: "pending" },
-  ]);
+    const [data, setData] = useState([
+        { id: 1, name: "Reusable", budget: "10000.0", status: "pending" },
+        { id: 4, name: "Reusable", budget: "10000.0", status: "pending" },
+        { id: 5, name: "Reusable", budget: "10000.0", status: "pending" },
+    ]);
 
-  const handleDelete = (id: number) => {
-    setData((prevData) => prevData.filter((item) => item.id !== id));
-  };
+    const handleDelete = (id: number) => {
+        setData((prevData) => prevData.filter((item) => item.id !== id));
+    };
 
-  const tableData = data.map((item) => ({
-    ...item,
-    delete: (
-      <FontAwesomeIcon 
-      icon={faTrash}
-      style={{color:'#ff0000'}}
-      className="hover:cursor-pointer"
-      onClick={() => handleDelete(item.id)}
-       />
-    ),
-  }));
+    const tableData = data.map((item) => ({
+        ...item,
+        delete: (
+            <div className="hover:cursor-pointer"
+                style={{ color: '#ff0000' }} onClick={() => handleDelete(item.id)}>
+                <FontAwesomeIcon
+                    icon={faTrash}
+                    style={{ marginRight: 5 }}
+                />
+                Delete
+            </div>
+        ),
+    }));
 
-  return (
-    <div className="flex grid grid-cols-12">
-      <UserSidepanel />
-      <div className="col-span-9 flex items-start justify-center mt-44">
-        <TableUser title="Your Projects" columns={columns} data={tableData} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex grid grid-cols-12">
+            <UserSidepanel />
+            <div className="col-span-9 flex items-start justify-center mt-44">
+                <TableUser title="Your Projects" columns={columns} data={tableData} />
+            </div>
+        </div>
+    );
 };
 
 export default UserDeleteView;
