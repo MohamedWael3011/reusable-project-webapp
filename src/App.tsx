ea238048c9d1dc18e7969d831165faf0c257b8c6
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2024-12-13 20:20:00 +0200

    auth integrations

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { HomeView } from "./views/HomeView";
 import AddThemePage from "./views/AddTheme_admin";
 import UpdateTheme from "./views/UpdateTheme_admin";
 import { AdminMainView } from "./views/AdminMainView";
-import { HomeView } from "./views/HomeView";import { UserMainView } from "./views/UserMainView";
+import { UserMainView } from "./views/UserMainView";
 
 const App = () => {
   return (
@@ -15,11 +15,11 @@ const App = () => {
       <Route path="/" element={<HomeView />} />
       <Route path="/signin" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/HomeView" element={<HomeView/>} />
-      <Route path="/AddTheme" element={<AddThemePage/>} />
-      <Route path="/UpdateTheme" element={<UpdateTheme/>} />
+      <Route path="/HomeView" element={<HomeView />} />
+      <Route path="/AddTheme" element={<AddThemePage />} />
+      <Route path="/UpdateTheme" element={<UpdateTheme />} />
       <Route path="/admin" element={<AdminMainView />} />
-      <Route path="/user" element={<UserMainView/>}/>
+      <Route path="/user" element={<UserMainView />} />
 
       {/* Redirect unmatched routes to Home */}
       <Route path="*" element={<Navigate to="/" replace />} />
